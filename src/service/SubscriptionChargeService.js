a6107f88d859cfa08b6f1bce08ee2bfaa32faabd
ppalludan <humentech@gmail.com>
2019-10-30 14:46:22 +0100

    subscription charge changes

---
+++ src/service/SubscriptionChargeService.js
@@ -0,0 +1,49 @@
+`use strict`;
+
+module.exports = class SubscriptionChargesService {
+  constructor(apiReference) {
+    this.api = apiReference
+    this.name = 'subscriptionCharges'
+
+  }
+
+  async list(opt) {
+
+    opt = opt || {}
+    opt.pagesize = opt.pagesize || 100
+    opt.pagenumber = opt.pagenumber || 1
+
+    return await this.api.get('subscriptioncharges', opt)
+  }
+
+  async query(opt) {
+    opt = opt || {}
+    var query = {}
+    query.pagesize = opt.pagesize || 100
+    query.pagenumber = opt.pagenumber || 1
+    
+    if (opt.SubscriptionId) {
+      query.$filter = `SubscriptionID eq guid'${opt.SubscriptionId}'`
+    }
+
+    // "ProductPlanCharges/all(o: o/SKU eq '{SKU HERE}')"
+
+    return await this.api.get('subscriptioncharges/query', query)
+  }
+
+  async set(subscriptionChargeId, obj) {
+    return await this.api.put(`subscriptioncharges/${subscriptionChargeId}`, null, obj)
+  }
+
+  async setPrice(subscriptionChargeId, price) {
+    return await this.set(subscriptionChargeId, {
+      CustomPrice: price
+    })
+  }
+
+  async setChargeDate(subscriptionChargeId, date) {
+    return await this.set(subscriptionChargeId, {
+      NextChargeDate: date
+    })
+  }
+}
--- src/service/SubscriptionService.js
+++ src/service/SubscriptionService.js
@@ -7,7 +7,7 @@ module.exports = class SubscriptionService {
   }
 
   async list(opt) {
-    
+
     opt = opt || {}
     opt.pagesize = opt.pagesize || 100
     opt.pagenumber = opt.pagenumber || 1
@@ -33,26 +33,25 @@ module.exports = class SubscriptionService {
   async createStandard(customerid, productplanid) {
 
     return (await this.createFullSubscription({
-      customerid:customerid,
-      productplanid:productplanid,
-      autorenew:true,
-      initialterminterval:12,
-      initialtermperiod:300,
-      initialterm:"12 months",
-      renewalterminterval:12,
-      renewaltermperiod:300,
-      renewalterm:"12 months"
+      customerid: customerid,
+      productplanid: productplanid,
+      autorenew: true,
+      initialterminterval: 12,
+      initialtermperiod: 300,
+      initialterm: "12 months",
+      renewalterminterval: 12,
+      renewaltermperiod: 300,
+      renewalterm: "12 months"
     }))
 
   }
 
   async createFullSubscription(subscription) {
 
-    subscription.autorenew = true
     if (!subscription.customerid) {
       throw new Error('Missing customerid')
     }
-    if(!subscription.productplanid){
+    if (!subscription.productplanid) {
       throw new Error('Missing productplanid')
     }
     return (await this.api.post('subscriptions', subscription))
@@ -60,31 +59,35 @@ module.exports = class SubscriptionService {
   }
 
   async activate(subscriptionId) {
-    if(subscriptionId) {
+    if (subscriptionId) {
       return (await this.api.put(`subscriptions/${subscriptionId}/activate`))
     }
     throw new Error('missing subscriptionId')
   }
+
   async close(subscriptionId) {
-    if(subscriptionId) {
+    if (subscriptionId) {
       return (await this.api.put(`subscriptions/${subscriptionId}/cancel`))
     }
     throw new Error('missing subscriptionId')
   }
+
   async expire(subscriptionId) {
-    if(subscriptionId) {
+    if (subscriptionId) {
       return (await this.api.put(`subscriptions/${subscriptionId}/expire`))
     }
     throw new Error('missing subscriptionId')
   }
+
   async hold(subscriptionId) {
-    if(subscriptionId) {
+    if (subscriptionId) {
       return (await this.api.put(`subscriptions/${subscriptionId}/hold`))
     }
     throw new Error('missing subscriptionId')
   }
+
   async resume(subscriptionId) {
-    if(subscriptionId) {
+    if (subscriptionId) {
       return (await this.api.put(`subscriptions/${subscriptionId}/hold`))
     }
     throw new Error('missing subscriptionId')
