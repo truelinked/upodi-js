1490b9a5dadcbb97935cb87492cf5c5fbf1b4c61
ppalludan <humentech@gmail.com>
2019-11-01 01:02:08 +0100

    added the subscriptionid and customerid to the query

--- src/service/SubscriptionService.js
+++ src/service/SubscriptionService.js
@@ -15,6 +15,20 @@ module.exports = class SubscriptionService {
     return await this.api.get('subscriptions', opt)
   }
 
+  async query(opt) {
+
+    opt = opt || {}
+
+    var query = {}
+    query.pagesize = opt.pagesize || 100
+    query.pagenumber = opt.pagenumber || 1
+
+    if (opt.CustomerID) {
+      query.$filter = `CustomerID eq guid'${opt.CustomerID}'`
+    }
+
+    return await this.api.get('subscriptions/query', query)
+  }
 
   async create() {
 
